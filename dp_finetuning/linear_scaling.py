296a46303647201eb972dd018f911406ca86dafb
Ashwinee Panda <ashwinee@princeton.edu>
2023-08-15 18:56:35 -0400

    added code for hparam search

---
+++ dp_finetuning/linear_scaling.py
@@ -0,0 +1,247 @@
+from utils import DATASET_TO_SIZE, parse_args, get_ds
+from prv_accountant import GaussianMechanism, PRVAccountant
+from prv_accountant.dpsgd import find_noise_multiplier
+
+import numpy as np
+import copy
+
+from submit_utils import launch_run
+
+args = parse_args()
+
+args.max_phys_bsz = min(args.max_phys_bsz, args.batch_size)
+n_accum_steps = max(1, np.ceil(args.batch_size / args.max_phys_bsz))
+print("GETTING DATASET")
+train_loader, test_loader = get_ds(args)
+args.batch_size = args.max_phys_bsz
+
+# CONSTANTS FOR HYPERPARAMETER SEARCH
+# epsilon values - if you're asking why these sum up to greater than one it's because we are not using basic composition (we are using Corollary 3.3 from "Gaussian Differential Privacy" by Dong et al.)
+# you will probably want to adjust these for your task, these are just some reasonable values for the current experiment
+eps_1 = 0.01
+eps_2 = 0.1
+eps_f = 0.97 
+
+eta_max = 1 * DATASET_TO_SIZE[args.dataset] / 50000 # original linear scaling rule; we've arbitrarily chosen the dataset size for CIFAR as the reference. For ImageNet this will set eta_max to ~25. Note that this assumes full-batch.
+t_max = 150 # feel free to expand this as large as you like if you have the resources, we didn't want to wait too long for the experiment to finish
+max_r = int(eta_max * t_max)
+n_runs = 3 # number of runs for each hyperparameter setting, you can increase this if you want
+rtol = 0.05 # tolerance when sampling r
+
+valid_etas = np.array([0.01] + list(np.arange(0.05, eta_max, 0.05)))
+valid_ts = np.arange(5, t_max, 5)
+
+searched_etas = []
+searched_ts = []
+searched_sigmas = []
+searched_accs = []
+
+# first hparam search
+etas = []
+ts = []
+
+while len(etas) < n_runs:
+    r = np.random.choice(np.arange(1, int(max_r//10), 1)) # hardcoded for current exp
+    found = False
+    for _ in range(1000): # Attempt 1000 times to find a valid pair
+        eta = np.random.choice(valid_etas)
+        t = np.random.choice(valid_ts)
+        if abs(eta * t - r) <= rtol * r:
+            etas.append(eta)
+            ts.append(t)
+            found = True
+            break
+    if not found:
+        print(f"Failed to find a valid pair for r = {r}. Retrying...if this happens too much you probably need to decrease the granularity of r")
+
+sigmas = []
+
+# compute sigmas for corresponding ts assuming eps_1 = 0.01 (modify as necessary)
+eps_1 = 0.01
+eps_minis = [eps_1] * n_runs
+for t, eps in zip(ts, eps_minis):
+    sigma = find_noise_multiplier(
+            sampling_probability=1,
+            num_steps=int(t),
+            target_epsilon=eps,
+            target_delta=args.delta, # don't worry about this, we're not doing basic composition anyways
+            eps_error=0.001,
+            mu_max=5000)
+    sigmas.append(sigma)
+
+configs = [{
+    'lr': eta,
+    't': t,
+    'sigma': sigma,
+} for eta, t, sigma in zip(etas, ts, sigmas)]
+
+for config in configs:
+    run_args = copy.deepcopy(args)
+    run_args.lr = config['lr']
+    run_args.epochs = config['t']
+    run_args.sigma = config['sigma']
+    run_args.epsilon = eps_1
+    print("Launching run with lr {}, t {}, sigma {}".format(config['lr'], config['t'], config['sigma']))
+    acc = launch_run(run_args, train_loader, test_loader) 
+    config['acc'] = acc
+    searched_etas.append(config['lr'])
+    searched_ts.append(config['t'])
+    searched_sigmas.append(config['sigma'])
+    searched_accs.append(acc)
+
+# sort by accuracy
+configs = sorted(configs, key=lambda x: x['acc'], reverse=True)
+print("Sorted configs for eps_1", configs)
+# get the best config
+best_config = configs[0]
+best_eta_1 = best_config['lr']
+best_t_1 = best_config['t']
+
+valid_etas = np.arange(best_eta_1, eta_max, 0.05)
+valid_ts = np.arange(best_t_1, t_max, 5)
+
+etas = []
+ts = []
+min_r = int(best_eta_1 * best_t_1)
+while len(etas) < n_runs:
+    r = np.random.choice(np.arange(min_r, int(max_r//5), 1)) # hardcoded
+    found = False
+    for _ in range(1000): # Attempt 1000 times to find a valid pair
+        eta = np.random.choice(valid_etas)
+        t = np.random.choice(valid_ts)
+        if abs(eta * t - r) <= rtol * r:
+            etas.append(eta)
+            ts.append(t)
+            found = True
+            break
+    if not found:
+        print(f"Failed to find a valid pair for r = {r}. Retrying...if this happens too much you probably need to decrease the granularity of r")
+
+sigmas = []
+
+
+eps_minis = [eps_2] * n_runs
+for t, eps in zip(ts, eps_minis):
+    sigma = find_noise_multiplier(
+            sampling_probability=1,
+            num_steps=int(t),
+            target_epsilon=eps,
+            target_delta=args.delta, # don't worry about this, we're not doing basic composition anyways
+            eps_error=0.001,
+            mu_max=5000)
+    sigmas.append(sigma)
+
+configs = [{
+    'lr': eta,
+    't': t,
+    'sigma': sigma,
+} for eta, t, sigma in zip(etas, ts, sigmas)]
+
+for config in configs:
+    run_args = copy.deepcopy(args)
+    run_args.lr = config['lr']
+    run_args.epochs = config['t']
+    run_args.sigma = config['sigma']
+    run_args.epsilon = eps_2
+    print("Launching run with lr {}, t {}, sigma {}".format(config['lr'], config['t'], config['sigma']))
+    acc = launch_run(run_args, train_loader, test_loader) 
+    config['acc'] = acc
+    searched_etas.append(config['lr'])
+    searched_ts.append(config['t'])
+    searched_sigmas.append(config['sigma'])
+    searched_accs.append(acc)
+
+# sort by accuracy
+configs = sorted(configs, key=lambda x: x['acc'], reverse=True)
+print("Sorted configs for eps_2", configs)
+# get the best config
+best_config = configs[0]
+best_eta_2 = best_config['lr']
+best_t_2 = best_config['t']
+
+# now compute linear interpolation 
+xs = [eps_1, eps_2]
+ys = [best_eta_1 * best_t_1, best_eta_2 * best_t_2]
+print("Doing linear interpolation with eps {}, rs {}".format(xs, ys))
+m, b = np.polyfit(xs, ys, 1)
+
+# find values in valid_etas, valid_ts that are within some rtol of the extrapolated value
+r_extrap = m * eps_f + b
+
+print("Extrapolated value of r: {}".format(r_extrap))
+if r_extrap > max_r: # this generally won't happen but just in case
+    print("Extrapolated value of r is outside of range, using max values")
+    final_eta = eta_max
+    final_t = t_max
+else:
+    final_eta = None
+    final_t = None
+
+    valid_etas = np.arange(best_eta_2, eta_max, 0.05)
+    valid_ts = np.arange(best_t_2, t_max, 5)
+
+    for i in range(1000):
+        eta = np.random.choice(valid_etas)
+        t = np.random.choice(valid_ts)
+        if abs(eta * t - r_extrap) <= rtol * r_extrap:
+            final_eta = eta
+            final_t = t
+            break
+    if final_eta is None:
+        print('failed to find extrapolated value, increasing rtol')
+    for i in range(1000):
+        eta = np.random.choice(valid_etas)
+        t = np.random.choice(valid_ts)
+        if abs(eta * t - r_extrap) <= rtol * 2 * r_extrap:
+            final_eta = eta
+            final_t = t
+            break
+    if final_eta is None:
+        print('failed to find extrapolated value; there is an issue!')
+        raise ValueError
+
+config = {
+    'lr': final_eta,
+    't': final_t,
+    'sigma': find_noise_multiplier(
+            sampling_probability=1,
+            num_steps=int(final_t),
+            target_epsilon=eps_f,
+            target_delta=args.delta, # don't worry about this, we're not doing basic composition anyways
+            eps_error=0.001,
+    mu_max=5000)
+}
+args.lr = config['lr']
+args.epochs = config['t']
+args.sigma = config['sigma']
+
+print("Launching run with lr {}, t {}, sigma {}".format(config['lr'], config['t'], config['sigma']))
+final_acc = launch_run(args, train_loader, test_loader)
+
+print("Final accuracy: {}".format(final_acc))
+searched_accs.append(final_acc)
+searched_etas.append(config['lr'])
+searched_ts.append(config['t'])
+searched_sigmas.append(config['sigma'])
+print("All searched etas: {}".format(searched_etas))
+print("All searched ts: {}".format(searched_ts))
+print("All searched sigmas: {}".format(searched_sigmas))
+print("All searched accs: {}".format(searched_accs))
+# compute mus for hyperparameter search
+mus = []
+for t, sigma in zip(searched_ts, searched_sigmas):
+    mu = np.sqrt(t)/sigma
+    mus.append(mu)
+
+# compute total mu
+mu = np.sqrt(sum([mu**2 for mu in mus]))
+
+mech = GaussianMechanism(1) 
+# override mu 
+mech.mu = mu
+
+_, eps, _ = PRVAccountant(mech, max_self_compositions=1, eps_error=0.001, # this computes a lower bound for eps, estimate and upper bound, and we use the estimate, you can change this to use the upper bound if you want 
+                                  delta_error=1e-6).compute_epsilon(args.delta, 1)
+
+print("Total privacy cost for final accuracy including the privacy cost of hyperparameter search ", eps)
+assert eps < 1.0, "Privacy cost for hyperparameter search is too high! You've done something wrong."
---
+++ dp_finetuning/submit_utils.py
@@ -0,0 +1,186 @@
+from finetune_classifier_dp import *
+from utils import *
+from fastDP import PrivacyEngine
+args = None
+len_test = None
+n_accum_steps = 1
+
+def best_correct(test_stats):
+    return max([test_stats[i] for i in test_stats if "_acc" in i])
+
+def get_classifier(model_dict):
+    """
+    this is a catastrophe
+    """
+
+    def handle_dp(args, key, val):
+        if args.disable_dp:
+            return val
+        else:
+            # return val._module
+            return val
+
+    def handle_average(args, key, val):
+        if key in ["ema", "swa"]:
+            return val.module
+        else:
+            return val
+
+    def handle_augmult(args, key, val):
+        if args.augmult > -1:
+            return val[1]
+        else:
+            return val
+
+    for key, val in model_dict.items():
+        model_dict[key] = handle_augmult(
+            args, key, handle_dp(args, key, handle_average(args, key, val))
+        )
+    return model_dict
+
+def print_test_stats(test_stats):
+    for key, val in test_stats.items():
+        print(f"Test Set: {key} : {val:.4f}")
+
+def do_test(model_dict, data, target, criterion, test_stats):
+    for key, model in model_dict.items():
+        model.eval()
+        output = model(data)
+        test_stats[key + "_loss"] += criterion(
+            output, target
+        ).item()  # sum up batch loss
+        pred = output.argmax(dim=1, keepdim=True)
+        test_stats[key + "_acc"] += (
+            pred.eq(target.view_as(pred)).sum().item() * 100 / len_test
+        )
+    return test_stats
+
+def test(args, model_dict, device, test_loader):
+    model_dict = get_classifier(model_dict)
+    test_stats = {key + "_loss": 0 for key in model_dict.keys()}
+    test_stats.update({key + "_acc": 0 for key in model_dict.keys()})
+    criterion = nn.CrossEntropyLoss()
+    with torch.no_grad():
+        for data, target in tqdm(test_loader):
+            data, target = data.to(device), target.to(device)
+            test_stats = do_test(model_dict, data, target, criterion, test_stats)
+    print_test_stats(test_stats)
+    return best_correct(test_stats)
+
+def train(args, model, device, train_loader, optimizer, privacy_engine, epoch):
+    model.train()
+    criterion = nn.CrossEntropyLoss()
+    losses = []
+    for epoch_step, (data, target, _) in enumerate(tqdm(train_loader)):
+        data, target = data.to(device), target.to(device)
+        loss = criterion(model(data), target)
+        loss.backward()
+        if ((epoch_step + 1) % n_accum_steps) == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+            losses.append(loss.detach().cpu().numpy())
+
+    print(f"Train Epoch: {epoch} \t Loss: {np.mean(losses):.6f}")
+    return np.mean(losses)
+
+def do_training(
+    model, ema_model, train_loader, test_loader, optimizer, privacy_engine, sched
+):
+    corrects = []
+    for epoch in range(1, args.epochs + 1):
+        if sched is not None:
+            sched.step()
+        train_loss = train(args, model, args.device, train_loader, optimizer, privacy_engine, epoch)
+        # only do test every 10 epochs, or at the end of training
+        if epoch % 10 == 0 or epoch == args.epochs:
+            new_correct = test(
+                args,
+                {
+                    "model": model,
+                    "ema": ema_model,
+                },
+                args.device,
+                test_loader,
+            )
+            corrects.append(new_correct)
+        ema_model.update_parameters(model)       
+    return corrects
+
+def setup_all(train_loader):
+    ### CREATE MODEL, OPTIMIZER AND MAKE PRIVATE
+    use_bias = False
+    model = nn.Linear(ARCH_TO_NUM_FEATURES[args.arch], args.num_classes, bias=use_bias).cuda()
+    if args.standardize_weights: # by default this should be true, setting the weights to zero is very important
+        # model.weight.data.normal_(mean=0.0, std=0.01)
+        # model.bias.data.zero_()
+        model.weight.data.zero_()
+        # trunc_normal_(model.weight, std=0.01)
+        if use_bias:
+            # model[1].bias.data.add_(-10.)
+            model.bias.data.add(-10.)
+    
+    from timm.optim import Lamb, AdamW
+    if args.optimizer == "sgd":
+        optimizer = torch.optim.SGD(
+            model.parameters(),
+            lr=args.lr,
+            weight_decay=args.weight_decay,
+            momentum=args.momentum,
+            nesterov=False,
+        )
+    elif args.optimizer == "lamb":
+        optimizer = Lamb(model.parameters(), lr=args.lr, weight_decay=0.0)
+    elif args.optimizer == "adam":
+        optimizer = AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+    privacy_engine = PrivacyEngine(
+        module=model,
+        batch_size=args.max_phys_bsz,
+        sample_size=DATASET_TO_SIZE[args.dataset],
+        epochs=args.epochs,
+        max_grad_norm=args.max_per_sample_grad_norm,
+        noise_multiplier=args.sigma,
+        target_epsilon=args.epsilon,
+        target_delta=args.delta,
+        accounting_mode="glw",
+        clipping_fn="Abadi",
+        clipping_mode="MixOpt",
+        clipping_style="all-layer",
+        loss_reduction="mean",
+    )
+    privacy_engine.attach(optimizer)
+
+    sched = None
+    ema_avg = (
+        lambda averaged_model_parameter, model_parameter, num_averaged: 0.1
+        * averaged_model_parameter
+        + 0.9 * model_parameter
+    )
+    ema_model = torch.optim.swa_utils.AveragedModel(model, avg_fn=ema_avg)
+    # ema_model = None
+    return model, ema_model, optimizer, privacy_engine, sched, train_loader
+
+def launch_run(run_args, train_loader, test_loader):
+    # if run_args.epsilon == 0.01:
+    #     return np.random.choice([75, 85, 90])
+    # elif run_args.epsilon == 0.05:
+    #     return np.random.choice([94, 95, 96])
+    # else:
+    #     return 99
+    global args
+    global len_test
+    args = run_args
+    len_test = len(test_loader.dataset)
+    model, ema_model, optimizer, privacy_engine, sched, train_loader = setup_all(
+            train_loader
+        )
+    corrects = do_training(
+            model,
+            ema_model,
+            train_loader,
+            test_loader,
+            optimizer,
+            privacy_engine,
+            sched,
+        )
+    return max(corrects)
